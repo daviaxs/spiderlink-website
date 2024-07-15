aba989985f95b650a99c3053d2546257302f0db2
daviaxs <cadwvk@gmail.com>
2024-07-15 13:06:27 -0300

    refactor: update font to Nunito

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Nunito } from 'next/font/google'
 import { Header } from '@/shared/components/header/Header'
+import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const nunito = Nunito({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'SpiderLink',
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={nunito.className}>
         <Header />
         {children}
       </body>
