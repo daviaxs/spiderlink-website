9832118658834e6d5be86d1bfc8472607c011cb7
daviaxs <cadwvk@gmail.com>
2024-07-15 15:36:42 -0300

    refactor: Update navigation link label in header component

--- src/shared/components/header/utils/components/navigation/Navigation.tsx
+++ src/shared/components/header/utils/components/navigation/Navigation.tsx
@@ -1,13 +1,20 @@
 import Link from 'next/link'
 import { navigationLinks } from '../../navigationLinks'
+import { DropdownMenuItem } from '@radix-ui/react-dropdown-menu'
 
 export function Navigation() {
   return (
     <nav>
-      <ul className="flex gap-4 text-black">
+      <ul className="flex flex-col gap-2 text-black">
         {navigationLinks.map(({ href, label }) => (
-          <Link key={label} href={href}>
-            <li className="font-bold">{label}</li>
+          <Link
+            key={label}
+            href={href}
+            className="p-2 hover:bg-black hover:bg-opacity-10 rounded-md"
+          >
+            <DropdownMenuItem className="font-bold outline-none">
+              {label}
+            </DropdownMenuItem>
           </Link>
         ))}
       </ul>
