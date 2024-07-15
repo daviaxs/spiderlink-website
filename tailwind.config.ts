62d2593bfa706a0ba2d6d3e26fc988fbe1a5be99
daviaxs <cadwvk@gmail.com>
2024-07-15 12:14:33 -0300

    chore: update tailwind.config.ts with new colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,19 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/shared/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    colors: {
+      white: '#fff',
+      black: '#0B0B0B',
+      purple: '#5918E1',
+    },
+  },
   plugins: [],
-};
-export default config;
+}
+export default config
