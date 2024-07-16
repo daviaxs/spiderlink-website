93186f796fdc87ff8c50efdf5c3cfce64598b5e0
daviaxs <cadwvk@gmail.com>
2024-07-16 15:06:12 -0300

    refactor: Update NotFound component with responsive design for smaller screens

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,11 +5,13 @@ export default function NotFound() {
   return (
     <main className="flex flex-col items-center justify-center w-full h-full gap-4">
       <div className="flex flex-col items-center justify-center">
-        <h1 className="font-black text-[4rem] leading-[100%]">
+        <h1 className="font-black text-[4rem] leading-[100%] max-sm:text-[3rem]">
           Erro 404 &#41;:
         </h1>
 
-        <p className="font-bold text-[1.3rem]">Página não encontrada</p>
+        <p className="font-bold text-[1.3rem] max-sm:text-[1rem]">
+          Página não encontrada
+        </p>
       </div>
 
       <Link href="/">
