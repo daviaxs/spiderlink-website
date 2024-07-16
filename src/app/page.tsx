537fa1052ff00c49b2ae5c815907b6ace59ef2b4
daviaxs <cadwvk@gmail.com>
2024-07-16 16:25:42 -0300

    refactor: Update Home page layout for better responsiveness and centered text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,12 @@ export default function Home() {
       <div className="flex items-center w-full justify-between gap-[6rem] max-lg:flex-col max-lg:gap-[4rem]">
         <div className="flex flex-col gap-[1.875rem] max-lg:text-center max-lg:items-center">
           <div>
-            <h1 className="font-black text-[3rem] leading-tight">
+            <h1 className="font-black text-[3rem] leading-tight max-sm:text-[2rem]">
               Descomplique a Gestão do Seu{' '}
               <span className="text-purple">Food Service.</span>
             </h1>
 
-            <p className="text-black font-bold text-[1rem] leading-tight opacity-90">
+            <p className="text-black font-bold text-[1rem] leading-tight opacity-90 max-sm:text-[0.8rem]">
               Transforme a presença online do seu restaurante com a SpiderLink,
               a plataforma que facilita a criação e gerenciamento do seu site de
               Food Service. Aumente a visibilidade e atraia mais clientes com
