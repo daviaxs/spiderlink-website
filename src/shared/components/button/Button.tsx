ab93c6823762d20cc031eee2aedb7c94f400f3f9
daviaxs <cadwvk@gmail.com>
2024-07-16 15:04:46 -0300

    refactor: Update Button component to accept custom className

--- src/shared/components/button/Button.tsx
+++ src/shared/components/button/Button.tsx
@@ -1,12 +1,19 @@
+import { cn } from '@/lib/utils'
 import { ButtonHTMLAttributes, ReactNode } from 'react'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode
+  className?: string | undefined
 }
 
-export function Button({ children }: ButtonProps) {
+export function Button({ className, children }: ButtonProps) {
   return (
-    <button className="flex items-center justify-center gap-3 bg-purple text-white font-extrabold py-3 px-4 rounded-[0.938rem]">
+    <button
+      className={cn(
+        'flex items-center justify-center gap-3 bg-purple text-white font-extrabold py-3 px-4 rounded-[0.938rem]',
+        className,
+      )}
+    >
       {children}
     </button>
   )
