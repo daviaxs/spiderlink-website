e142d1e99dce84890041533266ea268aca25fd50
daviaxs <cadwvk@gmail.com>
2024-07-18 13:33:25 -0300

    feat: Add SubTitle component

---
+++ src/shared/components/texts/SubTitle.tsx
@@ -0,0 +1,20 @@
+import { cn } from '@/lib/utils'
+import { HTMLAttributes, ReactNode } from 'react'
+
+interface SubTitleProps extends HTMLAttributes<HTMLHeadingElement> {
+  children: ReactNode
+  className?: string | undefined
+}
+
+export function SubTitle({ children, className }: SubTitleProps) {
+  return (
+    <h3
+      className={cn(
+        `font-black text-[1.5rem] max-sm:text-[1rem] leading-[110%]`,
+        className,
+      )}
+    >
+      {children}
+    </h3>
+  )
+}
