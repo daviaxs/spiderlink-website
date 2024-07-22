0be9a651a7d0827c33439da16cdcde7da12f91e1
daviaxs <cadwvk@gmail.com>
2024-07-22 14:05:01 -0300

    feat: Add Benefits02 component

---
+++ src/app/(home)/utils/Benefits02.tsx
@@ -0,0 +1,28 @@
+import { HandCoins, MonitorSmartphone, Smile, Wrench } from 'lucide-react'
+
+export const Benefits02 = [
+  {
+    icon: <HandCoins className="stroke-purple" />,
+    title: 'Preços Acessíveis',
+    description:
+      'Economize com nossos planos competitivos, sem comprometer a qualidade.',
+  },
+  {
+    icon: <Smile className="stroke-purple" />,
+    title: 'Facilidade de Uso',
+    description:
+      'Plataforma intuitiva, perfeita para quem não tem experiência técnica.',
+  },
+  {
+    icon: <Wrench className="stroke-purple" />,
+    title: 'Suporte Dedicado',
+    description:
+      'Ajuda rápida e eficiente sempre que você precisar, sem custos adicionais.',
+  },
+  {
+    icon: <MonitorSmartphone className="stroke-purple" />,
+    title: 'Mobilidade',
+    description:
+      'Seu site é responsivo, garantindo uma ótima experiência em qualquer dispositivo.',
+  },
+]
