53ec6c2231f0a27b4e576c8413efdb6287022fa0
daviaxs <cadwvk@gmail.com>
2024-07-22 15:50:34 -0300

    feat: Add Services

---
+++ src/app/(home)/utils/Services.tsx
@@ -0,0 +1,27 @@
+import SpiderLinkServiceImg001 from '@/shared/assets/imgs/spider-link-service-img-001.png'
+import SpiderLinkServiceImg002 from '@/shared/assets/imgs/spider-link-service-img-002.png'
+import SpiderLinkServiceImg003 from '@/shared/assets/imgs/spider-link-service-img-003.png'
+import SpiderLinkServiceImg004 from '@/shared/assets/imgs/spider-link-service-img-004.png'
+
+export const Services = [
+  {
+    img: SpiderLinkServiceImg001,
+    title: 'Teste Completo',
+    description: 'Acesse todas as funcionalidades',
+  },
+  {
+    img: SpiderLinkServiceImg002,
+    title: 'Melhorar o atendimento ao cliente',
+    description: 'Cancele a qualquer momento',
+  },
+  {
+    img: SpiderLinkServiceImg003,
+    title: 'Aumentar sua visibilidade online',
+    description: 'Tenha seu próprio site em instantes',
+  },
+  {
+    img: SpiderLinkServiceImg004,
+    title: 'Aumentar sua visibilidade online',
+    description: 'Ajuda disponível 24/7',
+  },
+]
