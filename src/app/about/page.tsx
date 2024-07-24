7b07f7dfa339641f71cd91969157534f4cda0850
daviaxs <cadwvk@gmail.com>
2024-07-24 16:50:28 -0300

    feat: Add card component for mission statement in About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,4 @@
+import { Card } from '@/shared/components/card'
 import { Text } from '@/shared/components/texts/Text'
 import { Title } from '@/shared/components/texts/Title'
 
@@ -15,6 +16,16 @@ export default function About() {
           online do seu Food Service.
         </Text>
       </div>
+
+      <Card.Root>
+        <Card.Title>Nossa Missão</Card.Title>
+
+        <Card.Description>
+          A SpiderLink tem como missão facilitar a presença online de pequenos
+          negócios do setor de Food Service. Acreditamos que toda empresa merece
+          um site de qualidade, independente do tamanho ou do orçamento.
+        </Card.Description>
+      </Card.Root>
     </>
   )
 }
