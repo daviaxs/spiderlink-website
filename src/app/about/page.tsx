59acd948f55ca5253786a11d6a114e71efbb70d5
daviaxs <cadwvk@gmail.com>
2024-07-25 16:33:41 -0300

    refactor: Update About page to use TitleH1 and add WhatWeDo component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,15 @@
 import { Card } from '@/shared/components/card'
 import { Text } from '@/shared/components/texts/Text'
-import { Title } from '@/shared/components/texts/Title'
+import { TitleH1 } from '@/shared/components/texts/TitleH1'
+import { TitleH2 } from '@/shared/components/texts/TitleH2'
+import { TitleH3 } from '@/shared/components/texts/TitleH3'
+import { WhatWeDo } from './utils/WhatWeDo'
 
 export default function About() {
   return (
     <>
       <div className="max-w-[600px] flex flex-col items-center gap-[1.875rem]">
-        <Title className="text-[3rem] leading-tight">
-          Conheça a SpiderLink
-        </Title>
+        <TitleH1>Conheça a SpiderLink</TitleH1>
 
         <Text className="opacity-90">
           Nesta página, você descobrirá o que motiva a SpiderLink, nosso foco em
@@ -26,6 +27,29 @@ export default function About() {
           um site de qualidade, independente do tamanho ou do orçamento.
         </Card.Description>
       </Card.Root>
+
+      <div className="flex flex-col gap-[1.875rem] w-full">
+        <TitleH2>O Que Fazemos</TitleH2>
+
+        <div className="flex flex-col items-center justify-start w-full gap-[1.875rem]">
+          {WhatWeDo.map(({ icon, title, description }) => (
+            <div
+              className="flex gap-7 bg-black bg-opacity-[3%] rounded-[1.25rem] w-full px-[1.875rem] py-[0.938rem]"
+              key={title}
+            >
+              <div className="flex items-center justify-center bg-purple bg-opacity-20 rounded-[1.25rem] min-w-[120px] min-h-[120px] max-w-[120px] max-h-[120px]">
+                {icon}
+              </div>
+
+              <div className="flex flex-col gap-2 mt-2">
+                <TitleH3 className="text-purple text-left">{title}</TitleH3>
+
+                <Text className="text-left opacity-90">{description}</Text>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   )
 }
