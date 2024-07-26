6fa69be9df37e715d3eecb8eb6a1075f48eebbe5
daviaxs <cadwvk@gmail.com>
2024-07-26 16:18:00 -0300

    refactor: Update WhatWeDo section titles to use lowercase letters

--- src/app/about/utils/WhatWeDo.tsx
+++ src/app/about/utils/WhatWeDo.tsx
@@ -3,25 +3,25 @@ import { Rocket, Smile, HeartHandshake, Award } from 'lucide-react'
 export const WhatWeDo = [
   {
     icon: <Rocket className="stroke-purple" size={50} />,
-    title: 'Soluções Acessíveis e de Alto Nível',
+    title: 'Soluções acessíveis e de alto nível',
     description:
       'Oferecemos ferramentas de qualidade para pequenos negócios, ajudando-os a criar uma presença online de destaque.',
   },
   {
     icon: <Smile className="stroke-purple" size={50} />,
-    title: 'Plataforma Fácil de Usar',
+    title: 'Plataforma fácil de usar',
     description:
       'Nossa plataforma é intuitiva e amigável, permitindo que você crie e gerencie seu site sem complicações.',
   },
   {
     icon: <HeartHandshake className="stroke-purple" size={50} />,
-    title: 'Recursos de Grandes Empresas',
+    title: 'Recursos de grandes empresas',
     description:
       'Disponibilizamos recursos avançados que normalmente estão acessíveis apenas para grandes empresas, agora ao seu alcance.',
   },
   {
     icon: <Award className="stroke-purple" size={50} />,
-    title: 'Site Profissional e Atraente',
+    title: 'Site profissional e atraente',
     description:
       'Com a SpiderLink, você pode ter um site bonito e funcional, projetado para atrair e envolver seus clientes.',
   },
