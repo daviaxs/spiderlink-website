fb3d22b778e1d7f33dab87d788c12ffd7f950896
daviaxs <cadwvk@gmail.com>
2024-07-26 17:00:17 -0300

    feat: Add WhyChooseUs component for displaying reasons to choose our services in About page

---
+++ src/app/about/utils/WhyChooseUs.tsx
@@ -0,0 +1,22 @@
+export const WhyChooseUs = [
+  {
+    title: 'Foco em pequenos negócios',
+    description:
+      'Entendemos as necessidades específicas dos pequenos negócios e trabalhamos para oferecer ferramentas que realmente fazem a diferença.',
+  },
+  {
+    title: 'Design moderno',
+    description:
+      'Nossos templates são desenhados para impressionar, com um design bonito e funcional que atrai mais clientes.',
+  },
+  {
+    title: 'Recursos de grandes empresas',
+    description:
+      'Disponibilizamos recursos avançados que normalmente estão acessíveis apenas para grandes empresas, agora ao seu alcance.',
+  },
+  {
+    title: 'Preço acessível',
+    description:
+      'Oferecemos planos competitivos que cabem no orçamento de qualquer pequeno negócio, garantindo um excelente custo-benefício.',
+  },
+]
