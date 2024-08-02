adb9cd36d98d9b5e3e02d245d9458575e02a2cb9
daviaxs <cadwvk@gmail.com>
2024-08-02 17:06:45 -0300

    feat: Update PlansPage to include SpiderLink logo in the header

--- src/app/plans/page.tsx
+++ src/app/plans/page.tsx
@@ -1,3 +1,19 @@
+import { SpiderLink } from '@/shared/assets/svgs/spiderlink'
+import Link from 'next/link'
+
 export default function PlansPage() {
-  return <h1>hello world</h1>
+  return (
+    <div>
+      <header
+        className="
+        flex justify-center items-center bg-white w-full h-[4.375rem] px-[1.25rem] py-[0.625rem]"
+      >
+        <Link href="/" className="flex items-center justify-center gap-2">
+          <SpiderLink />
+
+          <p className="font-bold text-[1.5rem]">SpiderLink</p>
+        </Link>
+      </header>
+    </div>
+  )
 }
